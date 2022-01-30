b730b7acff9e62245f9ed490821ba7538486199b
George Stavroulakis <g.stavroulakis@gmail.com>
2022-01-30 14:08:27 +0100

    keyboard inputs done

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -15,102 +15,158 @@ const calcObject = {
 	temp: [], //numbers are stored here
 	operation: "", //last operation is saved here
 	//standard ui functions
-	//add event listener to the number buttons
-	numberButtons() {
-		number_inputs.forEach((number) => {
-			number.addEventListener("click", (e) => {
-				if (input.value === "0") {
-					input.value = "";
-				}
-				input.value += number.textContent;
-			});
-		});
+	//add event listeners to the number buttons
+	numberButtons(number) {
+		if (input.value === "0") {
+			input.value = "";
+		}
+		input.value += number;
 	},
-	//end of add event listener to the number buttons
+	//end of add event listeners to the number buttons
 	//clear function
 	clear() {
-		clear.addEventListener("click", () => {
-			input.value = 0;
-			output.textContent = 0;
-			this.temp = [];
-			this.operation = "";
-		});
+		input.value = 0;
+		output.textContent = 0;
+		this.temp = [];
+		this.operation = "";
 	},
 	equals() {
-		equals.addEventListener("click", () => {
-			let result;
-			if (input.value === "0" && this.temp.length > 0) {
-				this.temp[1] = this.temp[0];
-			}
-			this.temp.push(input.value);
-			input.value = 0;
-			if (this.operation === "+") {
-				result = Number(this.temp[0]) + Number(this.temp[1]);
-				output.textContent = result;
-				return result;
-			}
-			if (this.operation === "-") {
-				result = Number(this.temp[0]) - Number(this.temp[1]);
-				output.textContent = result;
-				return result;
-			}
-			if (this.operation === "*") {
-				result = Number(this.temp[0]) * Number(this.temp[1]);
-				output.textContent = result;
-				return result;
-			}
-			if (this.operation === "/") {
-				result = Number(this.temp[0]) / Number(this.temp[1]);
-				output.textContent = result;
-				return result;
-			}
-		});
+		let result;
+		if (input.value === "0" && this.temp.length > 0) {
+			this.temp[1] = this.temp[0];
+		}
+		this.temp.push(input.value);
+		input.value = 0;
+		if (this.operation === "+") {
+			result = Number(this.temp[0]) + Number(this.temp[1]);
+			output.textContent = result;
+			return result;
+		}
+		if (this.operation === "-") {
+			result = Number(this.temp[0]) - Number(this.temp[1]);
+			output.textContent = result;
+			return result;
+		}
+		if (this.operation === "*") {
+			result = Number(this.temp[0]) * Number(this.temp[1]);
+			output.textContent = result;
+			return result;
+		}
+		if (this.operation === "/") {
+			result = Number(this.temp[0]) / Number(this.temp[1]);
+			output.textContent = result;
+			return result;
+		}
 	},
 	//end of standard ui functions
 	//math functions
 	addition() {
-		addition.addEventListener("click", () => {
-			this.temp.push(input.value);
-			this.operation = "+";
-			output.textContent = input.value;
-			input.value = "0";
-		});
+		this.temp.push(input.value);
+		this.operation = "+";
+		output.textContent = input.value;
+		input.value = "0";
 	},
 	subtraction() {
-		subtraction.addEventListener("click", () => {
-			this.temp.push(input.value);
-			this.operation = "-";
-			output.textContent = input.value;
-			input.value = "0";
-		});
+		this.temp.push(input.value);
+		this.operation = "-";
+		output.textContent = input.value;
+		input.value = "0";
 	},
 	multiplication() {
-		multiplication.addEventListener("click", () => {
-			this.temp.push(input.value);
-			this.operation = "*";
-			output.textContent = input.value;
-			input.value = "0";
-		});
+		this.temp.push(input.value);
+		this.operation = "*";
+		output.textContent = input.value;
+		input.value = "0";
 	},
 	division() {
-		division.addEventListener("click", () => {
-			this.temp.push(input.value);
-			this.operation = "/";
-			output.textContent = input.value;
-			input.value = "0";
-		});
+		this.temp.push(input.value);
+		this.operation = "/";
+		output.textContent = input.value;
+		input.value = "0";
 	},
 	//end of math functions
 };
 
 const calculator = () => {
-	calcObject.clear(); //enable clear function
-	calcObject.numberButtons(); //enable number buttons function
-	calcObject.equals(); // enable equals
-	calcObject.addition(); //enable addition function
-	calcObject.subtraction(); //enable addition function
-	calcObject.multiplication(); //enable multiplication function
-	calcObject.division(); //enable division function
+	//number buttons
+	number_inputs.forEach((number) => {
+		number.addEventListener("click", (e) => {
+			calcObject.numberButtons(number.textContent); //call number buttons function
+		});
+	});
+	//keyboard input numbers
+	document.addEventListener("keydown", (e) => {
+		//regex test case
+		if (/\d/g.test(e.key)) {
+			calcObject.numberButtons(e.key); //call number buttons function
+		}
+	});
+
+	//clear button
+	clear.addEventListener("click", () => {
+		calcObject.clear(); //call clear function
+	});
+	//clear input with keyboard, comes from hitting delete or backspace
+	document.addEventListener("keydown", (e) => {
+		if (e.key === "Backspace" || e.key === "Delete") {
+			calcObject.clear(); //call clear function
+		}
+	});
+
+	//equals button
+	equals.addEventListener("click", () => {
+		calcObject.equals(); // call equals
+	});
+	//equals input with keyboard, comes from enter
+	document.addEventListener("keydown", (e) => {
+		if (e.key === "Enter") {
+			calcObject.equals(); // call equals
+		}
+	});
+
+	//addition button
+	addition.addEventListener("click", () => {
+		calcObject.addition(); //call addition function
+	});
+	// addition button input with keyboard
+	document.addEventListener("keydown", (e) => {
+		if (e.key === "+") {
+			calcObject.addition(); //call addition function
+		}
+	});
+
+	//subtraction button
+	subtraction.addEventListener("click", () => {
+		calcObject.subtraction(); //call subtraction function
+	});
+	// subtraction button input with keyboard
+	document.addEventListener("keydown", (e) => {
+		if (e.key === "-") {
+			calcObject.subtraction(); //call subtraction function
+		}
+	});
+
+	//multiplication button
+	multiplication.addEventListener("click", () => {
+		calcObject.multiplication(); //call multiplication function
+	});
+	// multiplication button input with keyboard
+	document.addEventListener("keydown", (e) => {
+		if (e.key === "*") {
+			calcObject.multiplication(); //call multiplication function
+		}
+	});
+
+	//division button
+	division.addEventListener("click", () => {
+		calcObject.division(); //call division function
+	});
+	// division button input with keyboard
+	document.addEventListener("keydown", (e) => {
+		if (e.key === "/") {
+			calcObject.division(); //call division function
+		}
+	});
 };
 
 export { calculator };
